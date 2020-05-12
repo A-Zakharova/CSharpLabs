82670534ffcf02d8356d56bf0492fc5a4d156035
A-Zakharova <65176941+A-Zakharova@users.noreply.github.com>
2020-05-12 14:19:57 +0300

    Add files via upload

---
+++ Л2/NotEnglishAlphabet/NotEnglishAlphabet/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+namespace NotEnglishAlphabet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int amountofletters=0, indicator = 0;
+            string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            Console.WriteLine("Enter your line: ");
+            string str = Console.ReadLine();
+
+            for (int i = 0; i <= str.Length-1; i++)
+            {
+                if ((str[i] == str.ToUpper()[i])&&(str[i]!=' '))
+                {
+                    for (int j = 0; j <= alphabet.Length-1; j++)
+                    {
+                        if (str[i] == alphabet[j])
+                        {
+                            indicator = 0;
+                            break; 
+                        }
+                        else indicator = 1 ;
+                    }
+                    if (indicator == 1) amountofletters++;
+                }
+            }
+            Console.WriteLine("The number of capital letters of non-English alphabet =  " + amountofletters);
+        }
+    }
+}
---
+++ Л2/ReverseOrder/ReverseOrder/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ReverseOrder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            Console.WriteLine("Enter your line: ");
+            str = Console.ReadLine();
+
+            StringArray(str);
+        }
+        static void StringArray(string str)
+        {
+            string result = string.Join(" ", str.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries));
+            string[] temp = result.Split(' ');
+
+            for (int i = temp.Length - 1; i >= 0; i--)
+            {
+                Console.Write(temp[i] + " ");
+            }
+        }
+
+    }
+}
