1a5b83e33094343f9b7ed0be0bf79b4769e4e173
A-Zakharova <65176941+A-Zakharova@users.noreply.github.com>
2020-05-12 14:21:08 +0300

    Add files via upload

---
+++ Л3/Rooms3/Rooms3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Rooms3
+{
+    class Premise
+    {
+        public string Square { get; set; }
+        public Premise(string number, string name)
+        {
+            Square = number;
+
+            Console.WriteLine(name);
+            Console.WriteLine("Square: " + Square);
+        }
+    }
+    class Rooms : Premise
+    {
+        private int beds;
+        public Rooms(int beds, string number, string name) : base(number, name)
+        {
+            this.beds = beds;
+            Console.WriteLine("The number of beds " + beds + "\n");
+        }
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Rooms LivingRoom = new Rooms(2, "24.3 m^2", "Living room");
+            Rooms BedRoom = new Rooms(1, "18.7 m^2", "Bedroom");
+            Rooms ChildrenRoom = new Rooms(3, "10.9 m^2", "Children's room");
+        }
+    }
+}
