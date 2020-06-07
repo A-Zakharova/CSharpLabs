f6a1c84135518c573f8a60daa34b1979e53ca2e6
A-Zakharova <65176941+A-Zakharova@users.noreply.github.com>
2020-06-07 18:41:27 +0300

    Add files via upload

---
+++ Л4/4.1/CPU.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace Laba41
+{
+    [StructLayout(LayoutKind.Sequential)]
+    class CPU
+    {
+        public ushort ProcessorArchitecture;
+        public ushort Reserved;
+        public uint PageSize;
+        public IntPtr MinimumApplicationAddress;
+        public IntPtr MaximumApplicationAddress;
+        public IntPtr ActiveProcessorMask;
+        public uint NumberOfProcessors;
+        public uint ProcessorType;
+        public uint AllocationGranularity;
+        public ushort ProcessorLevel;
+        public ushort ProcessorRevision;
+    }
+}
---
+++ Л4/4.1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Laba41
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"About CPU:\n{System.CPUInform()}");
+            Console.WriteLine($"About memory:\n{System.MemoryInform()}");
+            Console.ReadKey();
+        }
+    }
+}
