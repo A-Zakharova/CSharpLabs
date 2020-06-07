ac7b837cb86f80a983ae5eee489218904eb469dc
A-Zakharova <65176941+A-Zakharova@users.noreply.github.com>
2020-06-07 21:25:00 +0300

    Add files via upload

---
+++ Л8/Rooms8/Rooms8/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TypeOfRooms[] array = new TypeOfRooms[5];
+            array[0] = new TypeOfRooms("Bedroom", "22.4 km^2", 2, (bedNum)1, 1, 0);
+            array[1] = new TypeOfRooms("Baby bedroom", "12.3 km^2", 1, (bedNum)0, 0, 1);
+            array[2] = new TypeOfRooms("Guest room", "24.7 km^2", 2, (bedNum)2, 1, 0);
+            array[3] = new TypeOfRooms("Kitchen", "15.6 km^2", 3, (bedNum)0, 1, 0);
+            array[4] = new TypeOfRooms("Children room", "35.9 km^2", 0, (bedNum)2, 4, 1);
+ 
+
+            for (int i = 0; i < 5; i++)
+            {
+                try
+                {
+                    if (array[i].getWindows() < 1)
+                    {
+                        throw new Exception();
+                    }
+                }
+                catch (Exception)
+                {
+                    array[i].checkWindows += (mes) => Console.ForegroundColor = ConsoleColor.Red;
+                }
+                
+                array[i].checkWindows += delegate (string mes)
+                {
+                    Console.WriteLine(mes);
+                };
+                array[i].getInfo();
+            }
+
+
+        }
+    }
+}
+
---
+++ Л8/Rooms8/Rooms8/TypeOfRooms.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class TypeOfRooms : Room
+    {
+        public struct parametre
+        {
+            public int numberOfTV;
+            public int numberOfbabybed;
+
+        }
+
+        public delegate void forEvent(string mes);
+        public event forEvent checkWindows;
+
+        public parametre roomStruct;
+
+        public TypeOfRooms() : this("Неизвестно", "Неизвестно", 0, 0, 0, 0)
+        {
+            ID = 0;
+        }
+        public TypeOfRooms(string a, string b, int c, bedNum f, int g, int h)
+        {
+            name = a;
+            square = b;
+            numberofwindows = c;
+            ID = generateID();
+            numberofbeds = (int)f;
+            roomStruct.numberOfTV = g;
+            roomStruct.numberOfbabybed = h;
+        }
+
+        public override void getInfo()
+        {
+            Console.WriteLine($"Имя: {name}");
+            Console.WriteLine($"Square: {square}");
+            Console.WriteLine($"Number of windows: {numberofwindows}");
+            Console.WriteLine($"Number of beds: {numberofbeds}");
+            Console.WriteLine($"Number of TV: {roomStruct.numberOfTV}");
+            Console.WriteLine($"Number of baby bed: {roomStruct.numberOfbabybed}");
+            Console.WriteLine($"ID: {ID}");
+            Console.WriteLine("");
+        }
+
+    
+    }
+}
